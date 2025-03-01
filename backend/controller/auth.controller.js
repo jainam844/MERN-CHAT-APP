10f36db798cd822aea323941b16fe833da1c97bb
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-01 14:24:53 +0530

    sign up

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,14 +1,53 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+
 export const login = (req, res) => {
     console.log("login User")
 }
-export const singup = (req, res) => {
+export const singup = async (req, res) => {
     try {
-        const { fullname, username, password, comfirmpassword, gender } = req.body;
-        
-    } catch (err) {
+        const { fullName, username, password, comfirmpassword, gender } = req.body;
+        if (password !== comfirmpassword) {
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
+        const user = await User.findOne({ username });
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender == 'male' ? boyProfilePic : girlProfilePic
+        })
+        if (newUser) {
+
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        }
+        else {
+            res.status(400).json({ error: "invalid user data" })
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
 
     }
 }
+
 export const logout = (req, res) => {
     console.log("logout User")
 }
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDb from "./db/connectToMongodb.js";
+
 const app = express();
-dotenv.config()
+dotenv.config();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-    res.send("hello world!!");
-});
+// app.get('/', (req, res) => {
+//     res.send("hello world!!");
+// });
 
 app.use('/api/auth', authRoutes)
 
