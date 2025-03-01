6d3f432deec5076692ce8a16fcaa9bdd2fdcc6c1
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-02 00:00:42 +0530

    send message completed

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { sendMessage } from "../controller/message.controller.js";
-import porotectRoute from "../middleware/protectRoute.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send/:id", porotectRoute, sendMessage);
+router.post("/send/:id", protectRoute, sendMessage);
 
 
 export default router;
