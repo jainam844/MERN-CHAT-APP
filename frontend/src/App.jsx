da117eaf5df725c80ea679231acebf9e5fde0917
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-02 14:43:13 +0530

    ROUTES ADDED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/SignUp";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <>
-      <div className="p-4 h-screen flex items-center justify-center">
-        <Home />
-      </div>
-    </>
+
+    <div className="p-4 min-h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+
   );
 }
 
