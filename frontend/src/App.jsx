12823907774b3a881065a8ae42c6c81a40407681
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-02 12:35:22 +0530

    added login signup and home pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
+import React from "react";
+import Home from "./pages/home/Home";
 
 function App() {
-
   return (
     <>
-      <div className='p-4 h-screen flex items-center justify-center'>
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Home />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
