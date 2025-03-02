8f33060649d527d98a3bec6fb664906cbb68ce9e
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-02 10:55:11 +0530

    get all users

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDb from "./db/connectToMongodb.js";
 
@@ -13,12 +14,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
-// app.get('/', (req, res) => {
-//     res.send("hello world!!");
-// });
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDb();
