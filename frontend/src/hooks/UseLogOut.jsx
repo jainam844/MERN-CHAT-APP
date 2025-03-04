5c11a16a6479731a85981efdb461b1b80245a4df
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-04 23:24:55 +0530

    LOGOUT

--- frontend/src/hooks/UseLogOut.jsx
+++ frontend/src/hooks/UseLogOut.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthContext } from '../context/AuthContext';
+import toast from 'react-hot-toast';
 
 const UseLogOut = () => {
-  return (
-    <div>UseLogOut</div>
-  )
+    const [loading, setLoading] = useState(false)
+    const { setAuthUser } = useAuthContext();
+
+    const logOut = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch("http://localhost:5000/api/auth/logout", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+            });
+            const data = await res.json();
+            if (data.error) {
+                throw new Error(data.error);
+            }
+            localStorage.removeItem("chat-user");
+            setAuthUser(null);
+
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+    return { loading, logOut };
 }
 
 export default UseLogOut
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -93,8 +93,8 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700' >
-              <span className=''>Sign Up</span>
+            <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+              {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
             </button>
           </div>
         </form>
