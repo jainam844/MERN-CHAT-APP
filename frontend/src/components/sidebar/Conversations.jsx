fe3aad1627c924314e21fa19aa91a1bf8ca8ed27
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-09 17:35:52 +0530

    send message completed

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -10,6 +10,7 @@ const Conversations = () => {
             {conversations.map((conversation, idx) => (
                 <Conversation key={conversation._id} conversation={conversation} emoji={getRandomEmoji()} lastIdx={idx === conversation.length - 1} />
             ))}
+            {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
         </div>
     );
 };
--- frontend/src/zustand/userConverstation.js
+++ frontend/src/zustand/userConverstation.js
@@ -3,6 +3,6 @@ const useConversation = create((set) => ({
     selectedConversation: null,
     setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
     messages: [],
-    setMessage: (messages) => set({ messages })
+    setMessages: (messages) => set({ messages })
 }))
 export default useConversation
