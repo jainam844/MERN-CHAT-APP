8390c6ce77769820cf33b4ae30cb4ffbd5e7c887
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-09 18:46:45 +0530

    get chat messages

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand/userConverstation';
+import { extractTime } from '../../utils/extractTime';
 
-const Message = () => {
+const Message = ({ message }) => {
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation();
+    const fromMe = message.senderId === authUser._id;
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? "chat-bubble-info" : "chat-bubble-secondary";
+    const shakeClass = message.shouldShake ? "shake" : "";
+    const formattedTime = extractTime(message.createdAt);
+
     return (
-        <div className="chat chat-end">
-            <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                    <img
-                        alt="Tailwind CSS chat bubble component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        <div className={`chat ${chatClassName}`}>
+            <div className='chat-image avatar'>
+                <div className='w-10 rounded-full'>
+                    <img alt='Tailwind CSS chat bubble component' src={profilePic} />
                 </div>
             </div>
-            <div className="chat-header">
-                Anakin
-                <time className="text-xs opacity-50">12:46</time>
-            </div>
-            <div className="chat-bubble">I hate you!</div>
-            <div className="chat-footer opacity-50">Seen at 12:46</div>
-        </div>)
+            <div className={`chat-bubble text-dark ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+            <div className='chat-footer text-dark  text-xs flex gap-1 items-center'>{formattedTime}</div>
+        </div>
+    )
 }
 
 export default Message
